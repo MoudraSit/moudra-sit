def855a208b435f4d1f2901ee111cda7ed554679
Rostislav Klein <rostaklein@gmail.com>
2023-04-17 21:30:10 +0200

    tabidoo API calls handler

---
+++ backend/tabidoo/index.ts
@@ -0,0 +1,43 @@
+type TabidooRequestParams = Partial<
+  Pick<Request, "method" | "body"> & {
+    urlParams: Record<string, string>;
+  }
+>;
+
+export async function callTabidoo<T = unknown>(
+  url: string,
+  { method = "GET", urlParams = {}, body }: TabidooRequestParams
+): Promise<T> {
+  const fullUrl = new URL(
+    `https://app.tabidoo.cloud/api/v2/apps/${process.env.TABIDOO_APP_NAME}${url}`
+  );
+
+  for (const [key, value] of Object.entries(urlParams)) {
+    fullUrl.searchParams.set(key, value);
+  }
+
+  fullUrl.search = fullUrl.search.replace("?", "");
+
+  const apiResponse = await fetch(fullUrl, {
+    method,
+    body,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.TABIDOO_API_KEY}`,
+    },
+  });
+
+  if (!apiResponse.ok) {
+    throw new Error("Tabidoo API call failed");
+  }
+
+  const responseJson = (await apiResponse.json()) as { data: T };
+
+  console.log(fullUrl.toString());
+
+  if (JSON.stringify(responseJson).includes("errors")) {
+    throw new Error("Tabidoo API call failed" + JSON.stringify(responseJson));
+  }
+
+  return responseJson.data;
+}
