ca88416a100f851498cf79159b69fb2a37083997
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-09-08 06:57:06 +0200

    Separate senior and assistant pages

---
+++ app/senior/page.tsx
@@ -0,0 +1,15 @@
+import Profile from "components/senior/profile";
+import Head from "next/head";
+
+function Page() {
+  return (
+    <>
+      <Head>
+        <title>Profil Seniora | Moudrá Síť App</title>
+      </Head>
+      <Profile />
+    </>
+  );
+}
+
+export default Page;
