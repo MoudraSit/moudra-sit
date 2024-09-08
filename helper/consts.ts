8eeef9fc8f6e98b8da89c31c6dc25f3b35927efa
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-09-08 08:11:47 +0200

    Add authorization for approved users in Tabidoo

---
+++ helper/consts.ts
@@ -0,0 +1,9 @@
+export enum Role {
+  SENIOR = "senior",
+  DA = "digitalni-asistent",
+}
+
+export enum AssistantStatus {
+  PENDING = "PENDING",
+  ACTIVE = "ACTIVE",
+}
--- pages/prihlaseni/index.tsx
+++ pages/prihlaseni/index.tsx
@@ -1,9 +1,27 @@
 import Footer from "components/layout/footer";
 import Layout from "components/layout/layout";
 import SignInSide from "components/sign-in/sign-in";
+import { Role } from "helper/consts";
+import { getSession } from "next-auth/react";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function SignInPage() {
+  const router = useRouter();
+
+  // when already signed-in, redirect to profile page
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        if (session.user?.role === Role.DA) {
+          return router.replace("/asistent");
+        }
+        router.replace("/senior");
+      }
+    });
+  }, [router]);
+
   return (
     <>
       <Layout />
