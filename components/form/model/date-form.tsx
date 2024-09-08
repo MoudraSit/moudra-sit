1263d3dd68e19fa3f64f0f2c99354acecef15824
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-09-08 11:00:43 +0200

    Silence type check

--- components/form/model/date-form.tsx
+++ components/form/model/date-form.tsx
@@ -1,25 +1,25 @@
 import { DateField, DateFieldProps } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { useField, useFormikContext } from "formik";
 import React from "react";
 
 type Props = {
   date: string;
   inputhelper: string;
-} & DateFieldProps;
+} & DateFieldProps<Date>;
 
 const DateForm = ({ inputhelper, ...props }: Props) => {
   const { setFieldValue } = useFormikContext();
-  const [field, meta] = useField(props);
+  // @ts-ignore
+  const [field, meta] = useField<any>(props);
 
   return (
     <DateField
       {...field}
       {...props}
-      selected={(field.value && new Date(field.value)) || null}
       onChange={(val) => {
         setFieldValue(field.name, val);
       }}
-      error={meta.touched && meta.error ? true : false}
       helperText={meta.touched && meta.error ? meta.error : inputhelper}
     />
   );
