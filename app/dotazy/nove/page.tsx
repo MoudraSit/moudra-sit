95a6998798e4594d6c54e2072a39e6ad56ddf45f
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-09-14 11:03:48 +0200

    Create virtualized list for senior requests

---
+++ app/dotazy/nove/page.tsx
@@ -0,0 +1,11 @@
+import NewRequestsList from "components/senior-requests/new-requests-list";
+
+function Page() {
+  return (
+    <>
+      <NewRequestsList />
+    </>
+  );
+}
+
+export default Page;
