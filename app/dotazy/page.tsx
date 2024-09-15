fa2dbbc718efe8d24d19782af677685ffd01b148
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-09-15 07:11:27 +0200

    Use Metadata objects in app pages

--- app/dotazy/page.tsx
+++ app/dotazy/page.tsx
@@ -1,15 +1,16 @@
 import SeniorRequestsWrapper from "components/senior-requests/senior-requests-wrapper";
-import Head from "next/head";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dotazy",
+};
 
 function Page() {
-  return (
-    <>
-      <Head>
-        <title>Dotazy | Moudrá Síť App</title>
-      </Head>
-      <SeniorRequestsWrapper />
-    </>
-  );
+  // TODO: parallel fetching of senior requests categories (new, my, ...) and loading on this page
+  // TODO: once routed to a specific page (my requests, ...), the fetch will be there too, but caching should take care of that (use the same function defined in a separate file)
+  // TODO: in specific pages, use virtualized lists to render only what is currently displayed
+
+  return <SeniorRequestsWrapper />;
 }
 
 export default Page;
