fdc14a94af6978f36b834157cc482280593a004f
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-09-15 08:38:25 +0200

    Add error boundaries

---
+++ app/not-found.tsx
@@ -0,0 +1,27 @@
+import { Button, Stack, Typography } from "@mui/material";
+import Link from "next/link";
+
+function Page() {
+  return (
+    <Stack spacing={2}>
+      <Typography variant="h1" sx={{ textAlign: "center", fontWeight: "bold" }}>
+        Stránka nenalezena
+      </Typography>
+      <Typography variant="subtitle1" sx={{ textAlign: "center" }}>
+        Tuto stránku se nepovedlo nalézt. Tlačítkem níže se dostanete na úvodní
+        stránku.
+      </Typography>
+
+      <Button
+        sx={{ alignSelf: "center" }}
+        variant="contained"
+        LinkComponent={Link}
+        href={"/dotazy"}
+      >
+        Zpět
+      </Button>
+    </Stack>
+  );
+}
+
+export default Page;
