3cfaeeac480722c3df87455c0c12c187a630ee0e
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-09-15 07:11:27 +0200

    Use Metadata objects in app pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,17 @@ import "styles/globals.css";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import { appTheme } from "components/theme/theme";
-import { SessionProvider } from "next-auth/react";
 import { SessionProviderWrapper } from "components/session-provider-wrapper/session-provider-wrapper";
 import ResponsiveAppBar from "components/layout/header";
-import Footer from "components/layout/footer";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s |  Moudrá Síť App",
+    default: "Aplikace", // a default is required when creating a template
+  },
+};
 
 export default function RootLayout({
   children,
