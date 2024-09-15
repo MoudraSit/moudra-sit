bccc21a1d6136465eb4433bddec70f2d0c17cf01
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-09-15 08:22:20 +0200

    Add navigation basics, add unified layout

--- app/dotazy/nove/page.tsx
+++ app/dotazy/nove/page.tsx
@@ -1,9 +1,18 @@
 import NewRequestsList from "components/senior-requests/new-requests-list";
+import { Typography } from "@mui/material";
+import { getNewSeniorRequests } from "backend/senior-requests";
+import BackButton from "components/buttons/back-button";
 
-function Page() {
+async function Page() {
+  const requests = await getNewSeniorRequests();
+
   return (
     <>
-      <NewRequestsList />
+      <BackButton href="/dotazy" />
+      <Typography variant="h5" sx={{ margin: "3px", fontWeight: "bold" }}>
+        Dotazy ({requests.length})
+      </Typography>
+      <NewRequestsList requests={requests} />
     </>
   );
 }
--- components/senior-requests/request-cards/for-handover-requests-card.tsx
+++ components/senior-requests/request-cards/for-handover-requests-card.tsx
@@ -1,15 +1,20 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardActionArea, CardContent, Typography } from "@mui/material";
 import * as React from "react";
 import { getForHandoverSeniorRequests } from "backend/senior-requests";
+import Link from "next/link";
 
 async function ForHandoverRequestsCard() {
   const requests = await getForHandoverSeniorRequests();
 
   return (
     <Card>
-      <CardContent>
-        <Typography variant="body2">Dotazy k předání {requests.length}</Typography>
-      </CardContent>
+      <CardActionArea LinkComponent={Link} href="/dotazy/k-predani">
+        <CardContent>
+          <Typography variant="body2">
+            Dotazy k předání ({requests.length})
+          </Typography>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 }
