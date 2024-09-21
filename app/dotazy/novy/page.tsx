e6efc7aefe12b33016a52ef98d066514f0272318
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-09-21 10:33:08 +0200

    Enhance BackButton to preserve history

--- app/dotazy/novy/page.tsx
+++ app/dotazy/novy/page.tsx
@@ -1,14 +1,13 @@
 import { Paper, Typography } from "@mui/material";
 import BackButton from "components/buttons/back-button";
 import NewQueryForm from "components/senior-requests/new-query-form";
-import { AssistantPagePaths } from "helper/consts";
 
 // TODO: prefill URL param that will use the query ID to prefill fields
 
 async function Page() {
   return (
     <>
-      <BackButton href={AssistantPagePaths.DASHBOARD} />
+      <BackButton />
       <Paper sx={{ padding: "0.75rem", marginTop: '1rem' }}>
         <Typography variant="h5" sx={{ margin: "3px" }}>
           Nový dotaz
