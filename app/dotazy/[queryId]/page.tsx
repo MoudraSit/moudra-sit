92cff8ddc51ea1a254a5fb215723683670649b1f
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-09-21 10:23:23 +0200

    Prepare /navstevy routes

---
+++ app/dotazy/[queryId]/page.tsx
@@ -0,0 +1,54 @@
+import { Button, Paper } from "@mui/material";
+import BackButton from "components/buttons/back-button";
+import { AssistantPagePaths } from "helper/consts";
+import { SeniorRequestsGetter } from "backend/senior-requests";
+import Link from "next/link";
+import { NotFoundError } from "helper/exceptions";
+import { redirect } from "next/navigation";
+import { SeniorRequest } from "types/seniorRequest";
+
+type Props = {
+  params: {
+    queryId: string;
+  };
+};
+
+async function Page({ params }: Props) {
+  const seniorRequestId = params.queryId;
+  let seniorRequest: SeniorRequest;
+  try {
+    seniorRequest = await SeniorRequestsGetter.getSeniorRequestById(
+      seniorRequestId
+    );
+  } catch (error) {
+    if (error instanceof NotFoundError) {
+      redirect("/404");
+    } else {
+      throw error;
+    }
+  }
+
+  return (
+    <>
+      <BackButton href={AssistantPagePaths.SENIOR_REQUESTS} />
+      <Paper>
+        <span>{seniorRequest.fields.popis}</span>
+        <Button
+          LinkComponent={Link}
+          // @ts-ignore
+          href={{
+            pathname: AssistantPagePaths.NEW_VISIT,
+            query: { queryId: seniorRequestId },
+          }}
+          fullWidth
+          variant="contained"
+          color="warning"
+        >
+          + Přidat návštěvu
+        </Button>
+      </Paper>
+    </>
+  );
+}
+
+export default Page;
