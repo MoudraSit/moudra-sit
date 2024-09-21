42e4f440b0679498450f63e6b7235544afe7bd49
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-09-21 11:01:08 +0200

    Separate my queries from query types

--- helper/consts.ts
+++ helper/consts.ts
@@ -9,6 +9,7 @@ export enum AssistantStatus {
   ACTIVE = "ACTIVE",
 }
 
+// The strings are horrible in the URL search params, but they are used in Tabidoo
 export enum SeniorRequestStatus {
   NEW = "00. Nový",
   FOR_HANDOVER = "01. K předání",
@@ -29,13 +30,7 @@ export enum SeniorPagePaths {
   SENIOR_PROFILE = "/senior",
 }
 
-// Request types are a custom UI category unlike request statuses defined in Tabidoo
-export enum SeniorRequestType {
-  NEW = "novy",
-  FOR_HANDOVER = "k-predani",
-  MINE = "moje",
-}
-
 export enum FilterType {
-  REQUEST_TYPE = "typDotazu",
+  QUERY_TYPE = "typDotazu",
+  USER_ASSIGNED = "jenMojeDotazy",
 }
