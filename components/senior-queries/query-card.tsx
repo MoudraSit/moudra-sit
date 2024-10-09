9c74e11f36acd5136e735968335424795dd07a16
Sebik3012Sk <sebik-kucera@hotmail.com>
2024-10-09 20:00:41 +0200

    Query Card Final Update - Part 1

--- components/senior-queries/query-card.tsx
+++ components/senior-queries/query-card.tsx
@@ -45,12 +45,14 @@ function QueryCard({ style, item , device }: Props) {
         }}
       />
 
-        <Typography fontSize={"21px"} variant="h2" fontWeight={"bold"}>{item.fields.popis}</Typography>
-        <Typography fontSize={"19px"} variant="body2">{item.fields.iDSeniora.fields.prijmeniJmeno}</Typography>
-        <Typography fontSize={"19px"} variant="body2">{item.fields.iDSeniora.fields.mesto}</Typography>
-        <Typography fontSize={"19px"} variant="body2">Zařízení : {device}</Typography>
-        <Typography fontSize={"19px"} fontWeight={"bold"} variant="body2">
-          Místo Pomoci : <span style={{fontWeight : "normal"}}>{}</span>
+        <Typography fontFamily={"Roboto Condensed"} fontSize={"21px"} variant="h2" fontWeight={"bold"}>{item.fields.popis}</Typography>
+        <Typography fontFamily={"Roboto Condensed"} fontSize={"19px"} variant="body2">{item.fields.iDSeniora.fields.prijmeniJmeno}</Typography>
+        <Typography fontFamily={"Roboto Condensed"} fontSize={"19px"} variant="body2">{item.fields.iDSeniora.fields.mesto}</Typography>
+        <Typography fontFamily={"Roboto Condensed"} fontSize={"19px"} variant="body2" fontWeight={"bold"}>
+          Zařízení : <span style={{fontWeight : "normal"}}>{device}</span>
+        </Typography>
+        <Typography fontFamily={"Roboto Condensed"} fontSize={"19px"} fontWeight={"bold"} variant="body2">
+          Místo Pomoci : <span style={{fontWeight : "normal",fontFamily : "Roboto Condensed"}}>{device}</span>
         </Typography>
         {/* <Typography variant="body2">{item.fields.pozadovaneMistoPomoci}</Typography> */}
         <CardActions>
@@ -61,7 +63,8 @@ function QueryCard({ style, item , device }: Props) {
             color="info"
             sx={
               {
-                width : "100%"
+                width : "100%",
+                fontFamily : "Roboto"
               }
             }
           >
