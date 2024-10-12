f759d778061cabca583f8b804b6f912f1a0b4ca6
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-10-12 11:27:43 +0200

    Add assistant score to form

---
+++ components/app-forms/FormHeadline.tsx
@@ -0,0 +1,19 @@
+import { Typography } from "@mui/material";
+
+function FormHeadline({ text }: { text: string }) {
+  return (
+    <Typography
+      sx={{
+        background: "#F4F4F4",
+        padding: "0.5rem",
+        fontSize: "16px",
+        fontWeight: "bold",
+      }}
+      variant="h2"
+    >
+      {text}
+    </Typography>
+  );
+}
+
+export default FormHeadline;
