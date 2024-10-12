68f92fc6b4534beaae975c76c69a541fa6ccc22d
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-10-12 09:30:21 +0200

    Add bottom drawer for comment adding

--- components/senior-queries/detail/new-comment-card.tsx
+++ components/senior-queries/detail/new-comment-card.tsx
@@ -1,44 +1,67 @@
 "use client";
 
-import { Box, Button, TextField } from "@mui/material";
+import {
+  Button,
+  CircularProgress,
+  Drawer,
+  Stack,
+  TextField,
+} from "@mui/material";
 import { createQueryComment } from "components/senior-queries/actions";
 import React from "react";
 
-function NewCommentCard({ queryId }: { queryId: string }) {
+type Props = {
+  queryId: string;
+  isOpen: boolean;
+  handleClose: Function;
+};
+
+function NewCommentCard({ queryId, isOpen, handleClose }: Props) {
   const [newComment, setNewComment] = React.useState("");
   const [isPending, setIsPending] = React.useState(false);
 
   async function handleNewComment() {
     setIsPending(true);
-    await createQueryComment(queryId, newComment);
     setNewComment("");
+    await createQueryComment(queryId, newComment);
     setIsPending(false);
+    handleClose();
   }
 
   return (
-    <Box>
-      <TextField
-        multiline
-        value={newComment}
-        onChange={(e) => setNewComment(e.target.value)}
-        fullWidth
-        color="info"
-        size="small"
-        placeholder="Nová poznámka"
-        minRows={3}
-        maxRows={6}
-        inputProps={{ style: { fontSize: "1rem" } }} // font size of input text
-      />
-      <Button
-        variant="text"
-        disabled={!newComment.length || isPending}
-        color="info"
-        onClick={handleNewComment}
-        sx={{ float: "right" }}
-      >
-        Přidat poznámku
-      </Button>
-    </Box>
+    <Drawer open={isOpen} onClose={() => handleClose()} anchor="bottom">
+      <Stack sx={{ padding: "1rem" }}>
+        {isPending ? (
+          <CircularProgress
+            color="secondary"
+            size={150}
+            sx={{ alignSelf: "center" }}
+          />
+        ) : (
+          <TextField
+            multiline
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+            fullWidth
+            color="info"
+            size="small"
+            placeholder="Nová poznámka"
+            minRows={6}
+            maxRows={10}
+            inputProps={{ style: { fontSize: "1rem" } }} // font size of input text
+          />
+        )}
+        <Button
+          variant="text"
+          fullWidth
+          disabled={!newComment.length || isPending}
+          color="info"
+          onClick={handleNewComment}
+        >
+          Přidat poznámku
+        </Button>
+      </Stack>
+    </Drawer>
   );
 }
 
