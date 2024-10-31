357f600d499f2f6fa14d44390d9f30d29d721d6a
Frantisek Pomkla <57173719+FenyCz@users.noreply.github.com>
2024-10-31 16:15:34 +0100

    Fix/rtg (#36)

--- components/scripts/sklik-rtg.tsx
+++ components/scripts/sklik-rtg.tsx
@@ -2,20 +2,26 @@ import Script from "next/script";
 
 function SklikRtg() {
   const customScript = ` 
-   window.sznIVA.IS.updateIdentities({
+  window.sznIVA.IS.updateIdentities({
     eid: null
   });
 
   var retargetingConf = {
     rtgId: 1524717,
-    consent: 0
+    consent: null
   };
   window.rc.retargetingHit(retargetingConf);
   `;
   return (
     <>
-      <Script type="text/javascript" src="https://c.seznam.cz/js/rc.js"></Script>
-      <Script id="sklik-rtg">{customScript}</Script>
+      <Script
+        type="text/javascript"
+        src="https://c.seznam.cz/js/rc.js"
+        strategy="lazyOnload"
+      ></Script>
+      <Script id="sklik-rtg" strategy="lazyOnload">
+        {customScript}
+      </Script>
     </>
   );
 }
