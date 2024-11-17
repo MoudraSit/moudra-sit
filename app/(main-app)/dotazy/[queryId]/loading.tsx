4a20fb901feaca8561595ccdf5c4ae68b7199d96
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-11-17 10:00:32 +0100

    Fix todos

--- app/(main-app)/dotazy/[queryId]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { CircularProgress, Grid } from "@mui/material";
-
-export default function Loading() {
-  return (
-    <Grid height={"50vh"} container justifyContent="center" alignItems="center">
-      <CircularProgress color="warning" size={100} />
-    </Grid>
-  );
-}
