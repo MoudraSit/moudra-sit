f5635925c3e52c6725e47909b5455b81bc219826
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-11-17 09:09:51 +0100

    Fix primary colors and datetime

--- app/(main-app)/dotazy/[queryId]/loading.tsx
+++ app/(main-app)/dotazy/[queryId]/loading.tsx
@@ -3,7 +3,7 @@ import { CircularProgress, Grid } from "@mui/material";
 export default function Loading() {
   return (
     <Grid height={"50vh"} container justifyContent="center" alignItems="center">
-      <CircularProgress color="secondary" size={100} />
+      <CircularProgress color="warning" size={100} />
     </Grid>
   );
 }
