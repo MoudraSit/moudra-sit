e17f46ba5308196e7cc32b45c67d3799eb2a7f6d
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-11-24 08:38:02 +0100

    Fix login image

--- components/sign-in/sign-in.tsx
+++ components/sign-in/sign-in.tsx
@@ -11,7 +11,7 @@ import { appTheme } from "components/theme/theme";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import Image from "next/image";
 import { signIn } from "next-auth/react";
-
+import loginImage from "public/images/sign-in/welcome.jpg";
 import logo from "public/images/logo/logo.svg";
 import { useRouter, useSearchParams } from "next/navigation";
 import { AssistantPagePaths } from "helper/consts";
@@ -56,12 +56,11 @@ function SignInSide() {
           style={{ position: "relative", overflow: "hidden" }}
         >
           <Image
-            src="/images/sign-in/welcome.jpg"
+            src={loginImage}
             alt="Uvodni foto - Moudra sit"
             style={{ objectFit: "cover" }}
             quality={75}
             fill
-            sizes="(min-width: 900px) 50vw"
             priority
           />
         </Grid>
