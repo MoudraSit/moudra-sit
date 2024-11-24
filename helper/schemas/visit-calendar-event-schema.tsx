0ba6765e68927b0b1d79c14f2153e6aab315eabf
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-11-24 10:06:09 +0100

    Add updating calendar events

--- helper/schemas/visit-calendar-event-schema.tsx
+++ helper/schemas/visit-calendar-event-schema.tsx
@@ -2,7 +2,9 @@ import * as yup from "yup";
 
 // schema for form validation
 export const visitCalendarEventSchema = yup.object({}).shape({
-  dateTime: yup.date().required(),
+  eventId: yup.string(),
+  seniorName: yup.string(),
+  dateTime: yup.string().required(),
   description: yup.string(),
   location: yup.string(),
 });
--- types/visit.ts
+++ types/visit.ts
@@ -24,5 +24,6 @@ export interface Visit {
     poznamkaSeniorem: string;
     poznamkaAsistentem: string;
     iDUzivatele: Assistant;
+    kalendarUdalostId: string;
   };
 }
