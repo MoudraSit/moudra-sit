b80e76008809e57a32d6d246dca2e7ef1faf4492
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-12-28 07:34:11 +0100

    Add login tests in CI pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,11 @@ import { defineConfig, devices } from "@playwright/test";
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
+export const BASE_URL = "http://localhost:3000";
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -26,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:3000",
+    baseURL: BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on",
@@ -58,16 +60,6 @@ export default defineConfig({
       name: "Mobile Safari",
       use: { ...devices["iPhone 12"] },
     },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,19 +1,42 @@
-import { test, expect } from "@playwright/test";
+import {
+  test,
+  expect,
+  chromium,
+  Browser,
+  BrowserContext,
+  Page,
+} from "@playwright/test";
+import { AUTH_ERROR_MESSAGE } from "app/lib/auth";
+import { BASE_URL } from "playwright.config";
 
 test.describe("/prihlaseni (Login Page)", () => {
-  test.beforeEach(async ({ page }) => {
+  let browser: Browser;
+  let context: BrowserContext;
+  let page: Page;
+
+  test.beforeEach(async () => {
+    // Slow motion is needed to show some of the alerts
+    browser = await chromium.launch({ headless: true, slowMo: 100 });
+    context = await browser.newContext();
+    page = await context.newPage();
+
     // Navigate to the login page before each test
     await page.goto("/prihlaseni");
   });
 
-  test("renders login page elements correctly", async ({ page }) => {
+  test.afterEach(async () => {
+    // Cleanup after each test
+    await browser.close();
+  });
+
+  test("renders login page elements correctly", async ({}) => {
     // Check if the page title and input fields are visible
     await expect(page.locator('input[name="email"]')).toBeVisible();
     await expect(page.locator('input[name="password"]')).toBeVisible();
     await expect(page.locator('button:has-text("Přihlásit se")')).toBeVisible();
   });
 
-  test("allows the user to toggle password visibility", async ({ page }) => {
+  test("allows the user to toggle password visibility", async ({}) => {
     const passwordField = page.locator('input[name="password"]');
     const toggleButton = page.locator(
       'button[aria-label="toggle password visibility"]'
@@ -31,7 +54,16 @@ test.describe("/prihlaseni (Login Page)", () => {
     await expect(passwordField).toHaveAttribute("type", "password");
   });
 
-  test("shows error for invalid credentials", async ({ page }) => {
+  test("shows error for invalid credentials", async ({}) => {
+    // Intercept reCAPTCHA API request and mock a success response
+    await page.route("/api/recaptcha", async (route) => {
+      route.fulfill({
+        status: 200,
+        contentType: "application/json",
+        body: JSON.stringify({ status: "success" }),
+      });
+    });
+
     const emailField = page.locator('input[name="email"]');
     const passwordField = page.locator('input[name="password"]');
     const loginButton = page.locator('button:has-text("Přihlásit se")');
@@ -44,37 +76,38 @@ test.describe("/prihlaseni (Login Page)", () => {
     await loginButton.click();
 
     // Expect error message to be displayed
-    const errorMessage = page.locator("text=Neočekávaná chyba");
-    await expect(errorMessage).toBeVisible();
+    const errorMessage = page.locator(`text=${AUTH_ERROR_MESSAGE}`);
+    await expect(errorMessage).toBeVisible({ timeout: 10_000 });
   });
 
-  test("redirects to dashboard on successful login", async ({ page }) => {
-    const emailField = page.locator('input[name="email"]');
-    const passwordField = page.locator('input[name="password"]');
-    const loginButton = page.locator('button:has-text("Přihlásit se")');
+  test("redirects to dashboard on successful login", async ({}) => {
+    const callbackUrl = `${BASE_URL}/prehled`;
 
-    // Fill in valid credentials
-    await emailField.fill("valid@example.com");
-    await passwordField.fill("correctpassword");
-
-    // Mock the successful login response
-    await page.route("**/api/auth/callback/credentials", (route) => {
+    // Intercept reCAPTCHA API request and mock a success response
+    await page.route("/api/recaptcha", async (route) => {
       route.fulfill({
         status: 200,
         contentType: "application/json",
-        body: JSON.stringify({ url: "/dashboard" }),
+        body: JSON.stringify({ status: "success" }),
       });
     });
 
-    // Click login button
-    await loginButton.click();
+    // Fill in valid login credentials
+    await page.fill('input[name="email"]', process.env.TEST_USERNAME as string);
+    await page.fill(
+      'input[name="password"]',
+      process.env.TEST_PASSWORD as string
+    );
+    await page.click('button:has-text("Přihlásit se")');
 
-    // Expect redirection to dashboard
-    await page.waitForURL("/dashboard");
-    await expect(page).toHaveURL("/dashboard");
+    // Wait for the redirection to the callbackUrl
+    await page.waitForURL(callbackUrl);
+
+    // Assert that the URL is correct
+    await expect(page).toHaveURL(callbackUrl);
   });
 
-  test("validates required fields", async ({ page }) => {
+  test("validates required fields", async ({}) => {
     const loginButton = page.locator('button:has-text("Přihlásit se")');
 
     // Click login without entering any credentials
