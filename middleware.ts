bd211d434dd0379904774efd4fa1d7b1574b1873
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2025-03-01 07:34:59 +0100

    Fix public images access

--- middleware.ts
+++ middleware.ts
@@ -60,5 +60,7 @@ export default withAuth(
 // Defined negatively via regex negative lookahead
 // Includes pages from the older app (mostly form and registration/login)
 export const config = {
-  matcher: ["/((?!form|newform|hodnoceni|api|prihlaseni|registrace|obnova-hesla).*)"],
+  matcher: [
+    "/((?!form|newform|hodnoceni|api|prihlaseni|registrace|obnova-hesla|images|_next|favicon.ico).*)",
+  ],
 };
