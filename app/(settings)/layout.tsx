4c3f9d805d0723b4687b05cad4e9350af24779b0
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2025-03-15 09:07:42 +0100

    Secure direct query routes

--- app/(settings)/layout.tsx
+++ app/(settings)/layout.tsx
@@ -4,13 +4,6 @@ import type { Metadata } from "next";
 import { Box } from "@mui/material";
 import { innerBoxStyles, outerBoxStyles } from "styles/common-layout";
 
-export const metadata: Metadata = {
-  title: {
-    template: "%s |  Moudrá Síť App",
-    default: "Aplikace", // a default is required when creating a template
-  },
-};
-
 export default function RootLayout({
   children,
 }: {
